8d8969059ed05385ac039f919fefdfe688f4c514
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-11-25 20:48:11 +0300

    add main html page

--- university/app/views.py
+++ university/app/views.py
@@ -1,5 +1,37 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
 
 def index(request):
-    return HttpResponse("Main page")
+    return render(request, 'index.html')
+
+
+def group(request):
+    return redirect('home')
+
+
+def study_info(request):
+    pass
+
+
+def department(request):
+    pass
+
+
+def personal_info(request):
+    pass
+
+
+def direction(request):
+    pass
+
+
+def teachers(request):
+    pass
+
+
+def parents(request):
+    pass
+
+
+def grade(request):
+    pass
