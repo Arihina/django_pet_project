18f1e0be8a5c1d2a0b3bce691a7407e5373f5dca
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-12-09 16:43:52 +0300

    upd admin

--- university/app/admin.py
+++ university/app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Parents, PersonalInfo, Direction, Teacher, Department, Group, Subject, Performance, TeacherSubject
+
 # Register your models here.
+admin.site.register(Parents)
+admin.site.register(PersonalInfo)
+admin.site.register(Direction)
+admin.site.register(Teacher)
+admin.site.register(Department)
+admin.site.register(Group)
+admin.site.register(Subject)
+admin.site.register(Performance)
+admin.site.register(TeacherSubject)
--- university/app/models.py
+++ university/app/models.py
@@ -2,120 +2,128 @@ from django.db import models
 
 
 class Parents(models.Model):
+    id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
+    Phone_number = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Родители'
-        verbose_name = "Parent"
-        verbose_name_plural = "Parents"
+        verbose_name = "Родитель"
+        verbose_name_plural = "Родители"
 
     def __str__(self):
         return self.full_name
 
 
 class PersonalInfo(models.Model):
+    id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=255)
-    gender = models.CharField(max_length=255)
-    birthday = models.DateField()
-    phone_number = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    parent = models.ForeignKey(Parents, on_delete=models.CASCADE)
-    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    Gender = models.CharField(max_length=255)
+    Birthday = models.DateField()
+    Phone_number = models.CharField(max_length=255)
+    Address = models.CharField(max_length=255)
+    id_parent = models.ForeignKey(Parents, on_delete=models.PROTECT, db_column='id_parent')
+    id_group = models.ForeignKey('Group', on_delete=models.PROTECT, db_column='id_group')
 
     class Meta:
         db_table = 'Личная_информация'
-        verbose_name = "Personal Info"
-        verbose_name_plural = "Personal Info"
+        verbose_name = "Личная информация"
+        verbose_name_plural = "Личная информация"
 
     def __str__(self):
         return self.full_name
 
 
 class Direction(models.Model):
-    short_name = models.CharField(max_length=255)
-    long_name = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
+    Short_name = models.CharField(max_length=255)
+    Long_name = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Направление'
-        verbose_name = "Direction"
-        verbose_name_plural = "Directions"
+        verbose_name = "Направление"
+        verbose_name_plural = "Направления"
 
     def __str__(self):
-        return self.short_name
+        return self.Long_name
 
 
 class Teacher(models.Model):
+    id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
+    Phone_number = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Преподаватели'
-        verbose_name = "Teacher"
-        verbose_name_plural = "Teachers"
+        verbose_name = "Преподаватель"
+        verbose_name_plural = "Преподаватели"
 
     def __str__(self):
         return self.full_name
 
 
 class Department(models.Model):
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    short_name = models.CharField(max_length=255)
-    long_name = models.CharField(max_length=255)
-    dean = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
+    id_teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT, db_column='id_teacher')
+    Short_name = models.CharField(max_length=255)
+    Long_name = models.CharField(max_length=255)
+    Decan = models.CharField(max_length=255)
+    Phone_number = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Кафедра'
-        verbose_name = "Department"
-        verbose_name_plural = "Departments"
+        verbose_name = "Кафедра"
+        verbose_name_plural = "Кафедры"
 
     def __str__(self):
-        return self.short_name
+        return self.Long_name
 
 
 class Group(models.Model):
-    direction = models.ForeignKey(Direction, on_delete=models.CASCADE)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    year = models.IntegerField()
-    form = models.CharField(max_length=255)
-    degree = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
+    id_direction = models.ForeignKey(Direction, on_delete=models.PROTECT, db_column='id_napravlenie')
+    id_department = models.ForeignKey(Department, on_delete=models.PROTECT, db_column='id_kafedra')
+    Year = models.IntegerField()
+    Form = models.CharField(max_length=255)
+    Degree = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Группа'
-        verbose_name = "Group"
-        verbose_name_plural = "Groups"
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
 
 
 class Subject(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'Предмет'
-        verbose_name = "Subject"
-        verbose_name_plural = "Subjects"
+        verbose_name = "Предмет"
+        verbose_name_plural = "Предметы"
 
     def __str__(self):
         return self.name
 
 
 class Performance(models.Model):
-    student = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    exam = models.IntegerField(null=True, blank=True)
-    credit = models.CharField(max_length=255, null=True, blank=True)
+    id = models.AutoField(primary_key=True)
+    id_student = models.ForeignKey(PersonalInfo, on_delete=models.PROTECT, db_column='id_student')
+    id_subject = models.ForeignKey(Subject, on_delete=models.PROTECT, db_column='id_object')
+    Exam = models.IntegerField(null=True, blank=True)
+    Zachet = models.CharField(max_length=255, null=True, blank=True)
 
     class Meta:
         db_table = 'Успеваемость'
-        verbose_name = "Performance"
-        verbose_name_plural = "Performances"
+        verbose_name = "Успеваемость"
+        verbose_name_plural = "Успеваемость"
 
 
 class TeacherSubject(models.Model):
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    id_teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT, db_column='id_teacher')
+    id_subject = models.ForeignKey(Subject, on_delete=models.PROTECT, db_column='id_object')
 
     class Meta:
         db_table = 'Преподаватель_Предмет'
-        verbose_name = "Teacher Subject"
-        verbose_name_plural = "Teacher Subjects"
+        verbose_name = "Преподаватель - Предмет"
+        verbose_name_plural = "Преподаватели - Предметы"
